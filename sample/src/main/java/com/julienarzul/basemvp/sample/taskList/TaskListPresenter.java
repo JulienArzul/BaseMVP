9498d9b88e930900b6c2478cd21dda1628a65dd2
jarzul <jarzul@2appaz.com>
2016-10-14 16:25:11 +0200

    Feat:(Sample) Datasource callback doesn't keep a reference on the view

--- sample/src/main/java/com/julienarzul/basemvp/sample/taskList/TaskListPresenter.java
+++ sample/src/main/java/com/julienarzul/basemvp/sample/taskList/TaskListPresenter.java
@@ -4,6 +4,7 @@ import com.julienarzul.basemvp.BasePresenter;
 import com.julienarzul.basemvp.sample.datasource.ITasksDatasource;
 import com.julienarzul.basemvp.sample.model.Task;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 /**
@@ -21,16 +22,46 @@ class TaskListPresenter extends BasePresenter<TaskListContract.View> implements 
 
     @Override
     public void loadData() {
-        this.tasksDatasource.getTaskList(new ITasksDatasource.DataCallback<List<Task>>() {
-            @Override
-            public void onDataLoaded(List<Task> data) {
-                view.setTaskList(data);
-            }
+        this.tasksDatasource.getTaskList(new TaskListDataCallback(this));
+    }
 
-            @Override
-            public void onDataNotAvailable() {
-                //TODO: Handle empty state
+    private void onTaskListLoaded(List<Task> taskList) {
+        if (this.view != null) {
+            this.view.setTaskList(taskList);
+        }
+    }
+
+    private void onTaskListEmpty() {
+        if (this.view != null) {
+            //TODO: Handle empty state
+        }
+    }
+
+    private static class TaskListDataCallback implements ITasksDatasource.DataCallback<List<Task>> {
+
+        private final WeakReference<TaskListPresenter> presenterWeak;
+
+        private TaskListDataCallback(TaskListPresenter presenter) {
+            this.presenterWeak = new WeakReference<>(presenter);
+        }
+
+        @Override
+        public void onDataLoaded(List<Task> data) {
+            TaskListPresenter presenter = this.presenterWeak.get();
+
+            if (presenter != null) {
+                presenter.onTaskListLoaded(data);
             }
-        });
+        }
+
+        @Override
+        public void onDataNotAvailable() {
+            TaskListPresenter presenter = this.presenterWeak.get();
+
+            if (presenter != null) {
+                presenter.onTaskListEmpty();
+            }
+        }
+
     }
 }
